41094db0e77ffb75c7d3079ce136c218648cee5b
Michael Witting <michael.witting@helmholtz-muenchen.de>
2021-03-24 10:22:52 +0100

    adding wrapper functions for forward and reverse dot product

--- R/utils_ms2.R
+++ R/utils_ms2.R
@@ -1,7 +1,44 @@
-forwardDp <- function(x) {
+forwardDp <- function(x, y, ...) {
+
+  compareSpectra(x,
+                 y,
+                 FUN = .mydotproduct,
+                 tolerance = tolerance,
+                 ppm = ppm,
+                 type = "outer")
 
 }
 
-backwardDp <- function(x) {
+backwardDp <- function(x, y, ...) {
+
+  compareSpectra(x,
+                 y,
+                 FUN = .mydotproduct,
+                 tolerance = tolerance,
+                 ppm = ppm,
+                 type = "right")
+
+}
+
+matchingPeaks <- function(x, y, ...) {
+
+  compareSpectra(x,
+                 y,
+                 FUN = .specNrow,
+                 tolerance = tolerance,
+                 ppm = ppm,
+                 type = "inner")
+
+}
+
+
+# costum matching function
+.specNrow <-  function(x, y, ...) {
+  nrow(x)
+}
+
+.mydotproduct <- function(x, y, type = NA, ...) {
+
+  MsCoreUtils::ndotproduct(x, y, ...)
 
 }
