207f20a5fa59db379df19f305404bce115422ab4
andreavicini <andrea1.vicini@mail.polimi.it>
2021-04-21 14:26:26 +0200

    matchMz and simple test

--- R/matchMz.R
+++ R/matchMz.R
@@ -70,6 +70,7 @@ setMethod("matchMz",
 
           })
 
+
 #' @rdname matchMz
 #'
 #' @importFrom MetaboCoreUtils adductNames
@@ -112,20 +113,162 @@ setMethod("matchMz", signature = c(x = "data.frame", y = "CompDb"),
           })
 
 
-
 .matchMz <- function(mz,
                      cmpds,
                      adducts,
                      tolerance = 0,
                      ppm = 0) {
-
+  
   # create data.frame will all ion m/z
   ionDf <- .createIonDf(cmpds, adducts)
-
+  
   # perform annotation of m/z values
   lapply(mz, .closestFeature, ionDf = ionDf, tolerance = tolerance, ppm = ppm)
-
+  
 }
+
+
+#' @importFrom MsCoreUtils closest
+#'
+#' @noRd
+.closestFeature <- function(mz,
+                            ionDf,
+                            tolerance = 0,
+                            ppm = 0) {
+  
+  # perform search based on mz
+  matches <- closest(ionDf$mz,
+                     mz,
+                     tolerance = tolerance,
+                     ppm = ppm) == 1
+  
+  matches[is.na(matches)] <- FALSE
+  
+  if(any(matches)) {
+    
+    return(ionDf[matches,])
+    
+  } else {
+    
+    return(NA)
+    
+  }
+}
+
+############################################################################
+############################################################################
+############################################################################
+############################################################################
+
+# Should I add also adducts to this class?
+
+setClass("TargetMass2MzParam", 
+         slots = c(
+           tolerance = "numeric",
+           ppm = "numeric"),
+         contains = "Param",
+         prototype = prototype(
+           tolerance = 0,
+           ppm = 5),
+         validity = function(object) {
+           msg <- NULL
+           if (length(object@tolerance) != 1 || object@tolerance < 0)
+             msg <- c("'tolerance' has to be a positive number of length 1")
+           if (length(object@ppm) != 1 || object@ppm < 0)
+             msg <- c("'ppm' has to be a positive number of length 1")
+           msg
+         })
+
+#' @rdname TargetMass2MzParam
+#' 
+#' @importFrom methods new
+#'
+#' @export
+TargetMass2MzParam <- function(tolerance = 0, ppm = 5) {
+  new("TargetMass2MzParam", tolerance = tolerance,
+      ppm = ppm)
+}
+
+
+#' @title m/z matching
+#'
+#' @name matchMz
+#'
+#' @description
+#'
+#' The `matchMz` method matches (compares) m/z values from a MS1 data table
+#'    with theoretical m/z values from compounds. `adducts` specifies the allowed
+#'    adducts based on adduct names from [MetaboCoreUtils::adductNames()]. The
+#'    maximum allowed error is defined by `tolerance` or `ppm` in `param`
+#'
+#' @param query Feature table containing information on MS1 features. Must contain
+#'    column called `mz`
+#'
+#' @param target Compound table with metabolites to compare against. Must contain
+#'    columns called `exactmass` and `name`
+#'
+#' @param param object containing information on tolerance (`numeric(1)` 
+#' for an absolute maximal accepted difference between m/z values.)
+#'   and ppm (`numeric(1)` for a relative, m/z-dependent, maximal accepted
+#'   difference between m/z values).
+#' @param adducts Allowed adducts named accordingly to [MetaboCoreUtils::adductNames()]
+#'
+#'
+#' @return `Matched` object representing the annotations
+#'
+#' @author 
+#'
+#' @export
+setGeneric("matchMz", function(query, target, param, adducts)
+  standardGeneric("matchMz")
+)
+
+
+setMethod("matchMz", 
+          signature = c(query = "data.frame", 
+                        target = "data.frame", 
+                        param = "TargetMass2MzParam"),
+          function(query, target, param, adducts = c("[M+H]+")){
+            # some sanity checks
+            if(!any(adducts %in% c(adductNames("positive"), adductNames("negative")))) {
+              
+              stop("Unknown adducts, please check MetaboCoreUtils for valid adducts")
+              
+            }
+            
+            if(!"mz" %in% colnames(query)) {
+              
+              stop("Missing mz column in query")
+              
+            }
+            
+            cmpds <- target
+            
+            if(!all(c("name", "exactmass") %in% colnames(cmpds))) {
+              
+              stop("Missing name and exactmass column in target")
+              
+            }
+            
+            mz <- query$mz
+            
+            
+            ionDf <- .createIonDf(cmpds, adducts)
+            
+            cls <- closest(ionDf$mz,
+                           mz,
+                           tolerance = param@tolerance,
+                           ppm = param@ppm)
+            mtchd <- which(!is.na(cls))
+            matches <- data.frame(query_idx = cls[mtchd],
+                                  target_idx = (as.integer(rownames(ionDf))
+                                                [mtchd] - 1L) %% nrow(cmpds) + 1L,
+                                  adduct = ionDf$adduct[mtchd],
+                                  score = mz[cls[mtchd]] - ionDf$mz[mtchd])
+            Matched(query = query, target = target, matches = matches)
+          })
+
+
 
 #' @importFrom MetaboCoreUtils mass2mz
 #'
@@ -146,31 +289,4 @@ setMethod("matchMz", signature = c(x = "data.frame", y = "CompDb"),
 
   ionDf[order(ionDf$mz),]
 
-}
-
-#' @importFrom MsCoreUtils closest
-#'
-#' @noRd
-.closestFeature <- function(mz,
-                            ionDf,
-                            tolerance = 0,
-                            ppm = 0) {
-
-  # perform search based on mz
-  matches <- closest(ionDf$mz,
-                     mz,
-                     tolerance = tolerance,
-                     ppm = ppm) == 1
-
-  matches[is.na(matches)] <- FALSE
-
-  if(any(matches)) {
-
-    return(ionDf[matches,])
-
-  } else {
-
-    return(NA)
-
-  }
 }
--- tests/testthat/test_matchMz.R
+++ tests/testthat/test_matchMz.R
@@ -21,3 +21,44 @@ test_that("MS1 annotation works", {
   expect_equal(ncol(result[[2]]), 5)
 
 })
+
+############################################################################
+############################################################################
+############################################################################
+############################################################################
+
+test_that("TargetMass2MzParam works", {
+  res <- TargetMass2MzParam()
+  expect_true(is(res, "TargetMass2MzParam"))
+  
+  expect_error(TargetMass2MzParam(tolerance = 1:3), "positive number")
+  expect_error(TargetMass2MzParam(ppm = -4), "positive number")
+})
+
+test_that("matchMz,TargetMass2MzParam works", {
+  
+  cmpds <- data.frame(
+    name = c("Tryptophan", "Leucine", "Isoleucine"),
+    formula = c("C11H12N2O2", "C6H13NO2", "C6H13NO2"),
+    exactmass = c(204.089878, 131.094629, 131.094629)
+  )
+  
+  adducts <- c("[M+H]+", "[M+Na]+")
+  
+  x <- data.frame(
+    mz = sort(c(205.097154, 132.101905))
+  )
+  
+  ionDf <- .createIonDf(cmpds, adducts)
+  
+  par <- TargetMass2MzParam(tolerance = 0, ppm = 20)
+  library(MetaboCoreUtils)
+  library(MsCoreUtils)
+  library(reshape2)
+  res <- matchMz(x, cmpds, par, adducts)
+  expect_equal(query(res), x) 
+  expect_equal(target(res), cmpds)
+  expect_equal(res@matches$query_idx, c(1, 1, 2))
+  expect_equal(res@matches$target_idx, c(1, 2, 1))
+  
+})
