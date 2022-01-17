84a32d988586e191cd82e9149513315aea3f1aa3
michaelwitting <michael.witting@helmholtz-muenchen.de>
2022-01-17 23:07:07 +0100

    initial functions for matching of formulas

---
+++ R/matchFormula.R
@@ -0,0 +1,120 @@
+#' @title formula matching
+#'
+#' @name matchFormula
+#'
+#' @description
+#'
+#' XXX
+#'
+#' @param query
+#'
+#' @param target
+#'
+#' @param ... currently ignored
+#'
+#' @return [Matched] object representing the result
+#'
+#' @author Michael Witting
+#'
+#' @examples
+#'
+#' XXX
+#'
+NULL
+
+#' @rdname matchFormula
+#'
+#' @export
+setGeneric("matchFormula", function(query, target, ...)
+  standardGeneric("matchFormula"))
+
+#' @rdname matchFormula
+#'
+#' @importFrom MetaboCoreUtils standardizeFormula
+setMethod("matchFormula",
+          signature = c(query = "character",
+                        target = "character"),
+          function(query, target, BPPARAM = SerialParam()) {
+            query <- unname(standardizeFormula(query))
+            target <- unname(standardizeFormula(target))
+            target_formula <- data.frame(index = rep(seq_along(target)),
+                                         formula = target)
+            matches <- do.call(
+              rbind, bpmapply(seq_along(query), query, FUN = .getFormulaMatches,
+                              MoreArgs = list(target = target_formula),
+                              BPPARAM = BPPARAM, SIMPLIFY = FALSE))
+            Matched(query = query, target = target, matches = matches)
+
+          })
+#' @rdname matchFormula
+setMethod("matchFormula",
+          signature = c(query = "data.frameOrSimilar",
+                        target = "data.frameOrSimilar"),
+          function(query, target, formulaColname = c("formula", "formula"),
+                   BPPARAM = SerialParam()) {
+            if(length(formulaColname) == 1)
+              formulaColname <- rep(formulaColname, 2)
+            if(!formulaColname[1] %in% colnames(query))
+              stop("Missing column \"", formulaColname[1], "\" in query")
+            if(!formulaColname[2] %in% colnames(target))
+              stop("Missing column \"", formulaColname[2], "\" in target")
+            res <- matchFormula(query[, formulaColname[1]],
+                                target[, formulaColname[2]])
+            res@query <- query
+            res@target <- target
+            res
+          })
+
+#' @rdname matchFormula
+setMethod("matchFormula",
+          signature = c(query = "character",
+                        target = "data.frameOrSimilar"),
+          function(query, target, formulaColname = "formula",
+                   BBPARAM = SerialParam()) {
+            if(!formulaColname %in% colnames(target))
+              stop("Missing column \"", formulaColname, "\" in target")
+            res <- matchFormula(query, target[, formulaColname])
+            res@query <- query
+            res@target <- target
+            res
+          })
+
+#' @rdname matchFormula
+setMethod("matchFormula",
+          signature = c(query = "data.frameOrSimilar",
+                        target = "character"),
+          function(query, target, formulaColname = "formula",
+                   BBPARAM = SerialParam()) {
+            if(!formulaColname %in% colnames(query))
+              stop("Missing column \"", formulaColname, "\" in query")
+            res <- matchFormula(query[, formulaColname], target)
+            res@query <- query
+            res@target <- target
+            res
+          })
+
+
+#' @author Michael Witting
+#'
+#' @param queryIndex `integer(1)` with the index of the query
+#'
+#' @param queryFormula `character(1)` with the formula of the query
+#'
+#' @param target `data.frame` with columns `"index"` and  `"formula"`
+#'
+#' @noRd
+.getFormulaMatches <- function(queryIndex, queryFormula, target) {
+
+  cls <- which(queryFormula == target$formula)
+
+  if(length(cls)) {
+    data.frame(query_idx = queryIndex,
+               target_idx = cls,
+               score = 1)
+  } else {
+    data.frame(query_idx = integer(),
+              target_idx = integer(),
+              score = numeric())
+  }
+}
+
