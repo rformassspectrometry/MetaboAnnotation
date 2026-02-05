849a99dce284644dac602623e84a82132895ff3f
Johannes Rainer <johannes.rainer@gmail.com>
2026-02-05 08:05:39 +0100

    refactor: replace msdata by MsDataHub
    - Load test data from the *MsDataHub* package removing the dependency on
      the *msdata* package.

--- R/validateMatchedSpectra.R
+++ R/validateMatchedSpectra.R
@@ -30,8 +30,8 @@
 #' @examples
 #'
 #' library(Spectra)
-#' ## Load test data
-#' fl <- system.file("TripleTOF-SWATH", "PestMix1_DDA.mzML", package = "msdata")
+#' ## Load test data from *MsDataHub*
+#' fl <- MsDataHub::PestMix1_DDA.mzML()
 #' pest_ms2 <- filterMsLevel(Spectra(fl), 2L)
 #' pest_ms2 <- pest_ms2[c(808, 809, 945:955)]
 #' load(system.file("extdata", "minimb.RData", package = "MetaboAnnotation"))
